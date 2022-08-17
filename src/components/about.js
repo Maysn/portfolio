6e6955eb27a872a616a677fc94d1f31b6ce133da
Maysn <mmazen1321@gmail.com>
2022-08-17 16:53:58 +0200

    update about me

--- src/components/about.js
+++ src/components/about.js
@@ -1,12 +1,19 @@
 export default function About(params) {
+  const age = () => {
+    return new Date().getFullYear() - 1997;
+  };
   return (
     <section id="About" className="about">
       <div className="about_cont">
         <h2 className="about_cont__title">About me</h2>
         <div className="about_cont__text">
           <p>
-            My name is Mazen, i'm a 23 years old guy from Egypt. I'm currently at the beginning of my journey as a frontend developer.
-            Getting into this career was a complete coincedence, when a friend talked to me about it, then i thought to give it a try since i wasn't really sure about what i wanted to do with my career, and i fell in love with coding ever since.
+            My name is Mazen, i'm a {age()} years old guy from Egypt. I'm
+            currently at the beginning of my journey as a frontend developer.
+            Getting into this career was a complete coincedence, when a friend
+            talked to me about it, then i thought to give it a try since i
+            wasn't really sure about what i wanted to do with my career, and i
+            fell in love with coding ever since.
           </p>
         </div>
       </div>
