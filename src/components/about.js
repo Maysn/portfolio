8e9c0896e978adcd285e2a42a54e2788fa7ff913
Maysn <mmazen1321@gmail.com>
2022-08-17 16:58:52 +0200

    update about me

--- src/components/about.js
+++ src/components/about.js
@@ -8,11 +8,11 @@ export default function About(params) {
         <h2 className="about_cont__title">About me</h2>
         <div className="about_cont__text">
           <p>
-            My name is Mazen, i'm a {age()} years old guy from Egypt. I'm
+            My name is Mazen, I'm a {age()} years old guy from Egypt. I'm
             currently at the beginning of my journey as a frontend developer.
             Getting into this career was a complete coincedence, when a friend
-            talked to me about it, then i thought to give it a try since i
-            wasn't really sure about what i wanted to do with my career, and i
+            talked to me about it, then I thought to give it a try since I
+            wasn't really sure about what I wanted to do with my career, and I
             fell in love with coding ever since.
           </p>
         </div>
