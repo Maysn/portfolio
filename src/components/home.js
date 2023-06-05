3a3a1adaed24cbf5cab8730ae078d97453056e2e
Maysn <mmazen1321@gmail.com>
2023-06-05 17:20:43 +0300

    update home section

--- src/components/home.js
+++ src/components/home.js
@@ -1,25 +1,28 @@
 import logo from "../assets/logot.png";
 
-
-function Home({open, loading}){
-    
-    return(
-        <section id="Home" className="home">
-        <div
-          className={
-            loading
-              ? "home__light"
-              : `${open ? "has-fade" : "home__light delay-light"}`
-          }
-          style={{ backgroundImage: `url(${logo})` }}
-        ></div>
-        <div className={loading? "home__introduction" : `${open? "has-fade" : "home__introduction"}`}>
-          <h1>Hey , I'm Mazen,</h1>
-          <p> a self-taught frontend developer based in Egypt.
-          </p>
-        </div>
-      </section>
-    );
+function Home({ open, loading }) {
+  return (
+    <section id="Home" className="home">
+      <div
+        className={
+          loading
+            ? "home__light"
+            : `${open ? "has-fade" : "home__light delay-light"}`
+        }
+        style={{ backgroundImage: `url(${logo})` }}
+      ></div>
+      <div
+        className={
+          loading
+            ? "home__introduction"
+            : `${open ? "has-fade" : "home__introduction"}`
+        }
+      >
+        <h1>Hey, I'm Mazen,</h1>
+        <p> a frontend developer based in Egypt.</p>
+      </div>
+    </section>
+  );
 }
 
 export default Home;
